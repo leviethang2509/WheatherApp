21e5df1a5260addcc3426219e8463e6a0948665b
ngaynaocunglaptrinh <quochoccode17082004@gmail.com>
2026-04-25 21:43:51 +0700

    Khởi tạo app va cau hinh API KEY

---
+++ lib/ models/weather_model.dart
@@ -0,0 +1,95 @@
+class WeatherModel {
+  final String cityName;
+  final String country;
+  final double temperature;
+  final double feelsLike;
+  final int humidity;
+  final double windSpeed;
+  final int pressure;
+  final String description;
+  final String icon;
+  final String mainCondition;
+  final DateTime dateTime;
+  final double tempMin;
+  final double tempMax;
+  final int visibility;
+  final int cloudiness;
+  final int sunrise;
+  final int sunset;
+
+  WeatherModel({
+    required this.cityName,
+    required this.country,
+    required this.temperature,
+    required this.feelsLike,
+    required this.humidity,
+    required this.windSpeed,
+    required this.pressure,
+    required this.description,
+    required this.icon,
+    required this.mainCondition,
+    required this.dateTime,
+    required this.tempMin,
+    required this.tempMax,
+    required this.visibility,
+    required this.cloudiness,
+    required this.sunrise,
+    required this.sunset,
+  });
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    return WeatherModel(
+      cityName: json['name'] ?? 'Unknown',
+      country: json['sys']?['country'] ?? '',
+      temperature: (json['main']?['temp'] ?? 0).toDouble(),
+      feelsLike: (json['main']?['feels_like'] ?? 0).toDouble(),
+      humidity: json['main']?['humidity'] ?? 0,
+      windSpeed: (json['wind']?['speed'] ?? 0).toDouble(),
+      pressure: json['main']?['pressure'] ?? 0,
+      description: json['weather']?[0]?['description'] ?? '',
+      icon: json['weather']?[0]?['icon'] ?? '01d',
+      mainCondition: json['weather']?[0]?['main'] ?? 'Clear',
+      dateTime: DateTime.fromMillisecondsSinceEpoch((json['dt'] ?? 0) * 1000),
+      tempMin: (json['main']?['temp_min'] ?? 0).toDouble(),
+      tempMax: (json['main']?['temp_max'] ?? 0).toDouble(),
+      visibility: json['visibility'] ?? 0,
+      cloudiness: json['clouds']?['all'] ?? 0,
+      sunrise: json['sys']?['sunrise'] ?? 0,
+      sunset: json['sys']?['sunset'] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': cityName,
+      'sys': {
+        'country': country,
+        'sunrise': sunrise,
+        'sunset': sunset,
+      },
+      'main': {
+        'temp': temperature,
+        'feels_like': feelsLike,
+        'humidity': humidity,
+        'pressure': pressure,
+        'temp_min': tempMin,
+        'temp_max': tempMax,
+      },
+      'wind': {
+        'speed': windSpeed,
+      },
+      'weather': [
+        {
+          'description': description,
+          'icon': icon,
+          'main': mainCondition,
+        }
+      ],
+      'dt': dateTime.millisecondsSinceEpoch ~/ 1000,
+      'visibility': visibility,
+      'clouds': {
+        'all': cloudiness,
+      },
+    };
+  }
+}
---
+++ lib/config/api_config.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+class ApiConfig {
+  static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static String get apiKey {
+    final key = dotenv.env['OPENWEATHER_API_KEY'];
+    if (key == null || key.isEmpty) {
+      throw Exception('OPENWEATHER_API_KEY is missing in .env file');
+    }
+    return key;
+  }
+  static const String currentWeather = '/weather';
+  static const String forecast = '/forecast';
+  static String buildUrl(String endpoint, Map<String, dynamic> params) {
+    final uri = Uri.parse('$baseUrl$endpoint');
+    final queryParams = {
+      ...params,
+      'appid': apiKey,
+      'units': 'metric',
+      'lang': 'vi',
+    };
+    return uri.replace(queryParameters: queryParams).toString();
+  }
+}
